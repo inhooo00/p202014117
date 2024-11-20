a70d3867aa60afd21907705cdcb2a3ff079b6b2e
inhooo00 <chldlsgh0987@naver.com>
2024-11-20 09:48:28 +0900

    two

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("202014117 최인호");
+        System.out.println(Hello.sum(10));
+        System.out.println(Hello.sum(100));
     }
 }
