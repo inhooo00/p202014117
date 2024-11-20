f97734c11921f20868bcec79d01cea24b1f3cf98
inhooo00 <chldlsgh0987@naver.com>
2024-11-20 09:49:39 +0900

    three

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("202014117 최인호");
         System.out.println(Hello.sum(10));
         System.out.println(Hello.sum(100));
+        System.out.println(Hello.sum(1000));
     }
 }
